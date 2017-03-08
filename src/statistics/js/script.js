ecfd3a7cdea09c07c29a5d6659b73cd9b6c49632
joacim.boive@gmail.com <gsxr750>
2017-03-08 15:21:15 +0100

    Reporting is working, including sub-tasks.

--- src/statistics/js/script.js
+++ src/statistics/js/script.js
@@ -1,21 +1,22 @@
 'use strict';
 {
 
+    let headers = new Headers();
+    let lookups = [];
+
     let toBase64 = (input) => {
         return window.btoa(decodeURIComponent(encodeURIComponent(input)));
     };
 
     let getLoggedHours = (storage) => {
-        const headers = new Headers({
-            'Authorization': 'Basic ' + toBase64(storage.login + ':' + storage.password),
-            'Content-Type': 'application/json'
-        });
+        headers.append('Authorization', 'Basic ' + toBase64(storage.login + ':' + storage.password));
+        headers.append('Content-Type', 'application/json');
 
         return new Promise((resolve, reject) => {
             storage.fromDate = document.getElementById('dateFrom').value;
             storage.toDate = document.getElementById('dateTo').value;
 
-            let workLogDates = ` and worklogDate >= ${storage.fromDate} & worklogDate <= ${storage.toDate}`;
+            let workLogDates = ` and worklogDate >= ${storage.fromDate} and worklogDate <= ${storage.toDate}`;
 
             fetch(`${storage.url}/rest/api/2/search?jql=${storage.jql + workLogDates}&maxResults=1000`, {
                 method: 'GET',
@@ -26,7 +27,6 @@
                     return response.json()
                 })
                 .then(jiras => {
-                    let lookups = [];
 
                     storage.fromDate = new Date(storage.fromDate);
                     storage.toDate = new Date(storage.toDate);
@@ -36,49 +36,13 @@
 
                     for (let issue of jiras.issues) {
                         if (issue.fields.issuetype.subtask === false) {
-                            lookups.push(fetch(issue.self, {
-                                    method: 'GET',
-                                    redirect: 'follow',
-                                    headers: headers
-                                }).then(response => {
-                                    return response.json()
-                                })
-                                    .then(issueDetails => {
-                                        let thisDate = '';
-                                        let key = issueDetails.key;
-
-                                        for (let log of issueDetails.fields.worklog.worklogs) {
-                                            thisDate = new Date(log.updated.split('T')[0]);
-
-                                            if (thisDate >= storage.fromDate && thisDate <= storage.toDate) {
-                                                if (!storage.report.issues[key]) {
-                                                    storage.report.issues[key] = {};
-                                                    storage.report.issues[key].details = {};
-                                                    storage.report.issues[key].data = [];
-                                                }
-
-                                                storage.report.issues[key].details.key = key;
-                                                storage.report.issues[key].details.status = issueDetails.fields.status.name;
-                                                storage.report.issues[key].details.summary = issueDetails.fields.summary;
-
-                                                storage.report.total += log.timeSpentSeconds;
-
-                                                storage.report.issues[key].data.push({
-                                                    'updated': log.updated,
-                                                    'displayName': log.updateAuthor.displayName,
-                                                    'timeSpentSeconds': log.timeSpentSeconds
-                                                })
-                                            }
-                                        }
-                                    })
-                            )
+                            lookups.push(getDetails(issue.self, storage));
                         }
                     }
 
-
                     Promise.all(lookups).then(() => {
                         console.table(storage.report);
-                        resolve(storage.report);
+                        resolve(storage);
                     }, reason => {
                         debugger;
                         console.log(reason);
@@ -88,22 +52,67 @@
         })
     };
 
-    let createReport = (data) => {
+    let getDetails = (url, storage) => {
+        return fetch(url, {
+            method: 'GET',
+            redirect: 'follow',
+            headers: headers
+        }).then(response => {
+            return response.json()
+        })
+            .then(issueDetails => {
+                let thisDate = '';
+                let key = issueDetails.key;
+
+                for (let log of issueDetails.fields.worklog.worklogs) {
+                    thisDate = new Date(log.updated.split('T')[0]);
+
+                    if (thisDate >= storage.fromDate && thisDate <= storage.toDate) {
+                        if (!storage.report.issues[key]) {
+                            storage.report.issues[key] = {};
+                            storage.report.issues[key].details = {};
+                            storage.report.issues[key].data = [];
+                        }
+
+                        storage.report.issues[key].details.key = key;
+                        storage.report.issues[key].details.type = issueDetails.fields.issuetype.name;
+                        storage.report.issues[key].details.status = issueDetails.fields.status.name;
+                        storage.report.issues[key].details.summary = issueDetails.fields.summary;
+
+                        storage.report.total += log.timeSpentSeconds;
+
+                        storage.report.issues[key].data.push({
+                            'updated': log.updated,
+                            'displayName': log.updateAuthor.displayName,
+                            'timeSpentSeconds': log.timeSpentSeconds
+                        })
+                    }
+                }
+
+                for(let task of issueDetails.fields.subtasks){
+                    debugger;
+                    lookups.push(getDetails(task.self, storage));
+                }
+            });
+    };
+
+    let createReport = (storage) => {
+        let data = storage.report;
         let holder = document.getElementById('holder');
         let loading = document.getElementById('loading');
         let total = 0;
-        let html = '<h3>Total hours for period: <span id="total"></span></h3><table class="table table-striped table-bordered table-hover"><tr><th>ID</th><th>Summary</th><th>Status</th><th>User</th><th>Date</th><th>Hours</th></tr>';
+        let html = '<h3>Total hours for period: <span id="total"></span></h3><table id="report" class="table table-striped table-bordered table-hover"><tr><th>ID</th><th>Type</th><th>Summary</th><th>Status</th><th>User</th><th>Date</th><th>Hours</th></tr>';
 
         for (let [key, value] of Object.entries(data)) {
             if (key === 'total') {
                 total = value;
             } else {
                 for (let [key, data] of Object.entries(value)) {
-                    const row = `<tr><td>${key}</td><td>${data.details.summary}</td><td>${data.details.status}</td>`;
+                    const row = `<tr><td><a href="${storage.url}/browse/${key}" target="_blank" rel="noopener">${key}</a></td><td>${data.details.type}</td><td>${data.details.summary}</td><td>${data.details.status}</td>`;
                     let thisDetails = '';
 
                     for (let log of data.data) {
-                        thisDetails += `${row}<td>${log.displayName}</td><td>${log.updated}</td><td>${log.timeSpentSeconds/3600}</td></tr>`;
+                        thisDetails += `${row}<td>${log.displayName}</td><td>${log.updated}</td><td>${log.timeSpentSeconds / 3600}</td></tr>`;
                     }
 
                     html += thisDetails;
@@ -112,7 +121,6 @@
         }
 
         html += '</tr></table>';
-debugger;
         holder.innerHTML = html;
         document.getElementById('total').innerHTML = parseInt(data.total) / 3600;
 
@@ -123,8 +131,16 @@ debugger;
 
     let getStorage = () => {
         let loading = document.getElementById('loading');
+        let holder = document.getElementById('holder');
 
+        holder.classList.add('hide');
+        holder.classList.remove('bounceIn');
+
+        loading.classList.remove('bounceOut');
         loading.classList.remove('hide');
+
+        headers = new Headers();
+        lookups = [];
 
         chrome.storage.local.get({
             'url': '',
