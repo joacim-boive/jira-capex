1d954d7d91b584c932a127cc0fd14b3b64f98a5a
joacim.boive@gmail.com <gsxr750>
2017-03-09 15:57:14 +0100

    Fixed notification and date/time-format

--- src/statistics/js/script.js
+++ src/statistics/js/script.js
@@ -18,12 +18,17 @@
 
             let workLogDates = ` and worklogDate >= ${storage.fromDate} and worklogDate <= ${storage.toDate}`;
 
+            storage.jiraReview = storage.jql + workLogDates;
+
             fetch(`${storage.url}/rest/api/2/search?jql=${storage.jql + workLogDates}&maxResults=1000`, {
                 method: 'GET',
                 redirect: 'follow',
                 headers: headers
             })
                 .then(response => {
+                    if (!response.ok) {
+                        throw Error(response.statusText);
+                    }
                     return response.json()
                 })
                 .then(jiras => {
@@ -49,6 +54,16 @@
                         reject(reason);
                     })
                 })
+                .catch(function(err) {
+                    notify({
+                        icon: 'glyphicon-exclamation-sign',
+                        type: 'danger',
+                        title: 'Huston! We have a problem!',
+                        message: err.message + ' - Please try again!'
+                    });
+
+                    console.log(err);
+                });
         })
     };
 
@@ -82,51 +97,54 @@
                         storage.report.total += log.timeSpentSeconds;
 
                         storage.report.issues[key].data.push({
-                            'updated': log.updated,
+                            'updated': log.updated.replace('T', ' ').split('.000')[0],
                             'displayName': log.updateAuthor.displayName,
                             'timeSpentSeconds': log.timeSpentSeconds
                         })
                     }
                 }
 
-                for(let task of issueDetails.fields.subtasks){
-                    debugger;
+                for (let task of issueDetails.fields.subtasks) {
                     lookups.push(getDetails(task.self, storage));
                 }
             });
     };
 
     let createReport = (storage) => {
-        let data = storage.report;
-        let holder = document.getElementById('holder');
-        let loading = document.getElementById('loading');
-        let total = 0;
-        let html = '<h3>Total hours for period: <span id="total"></span></h3><table id="report" class="table table-striped table-bordered table-hover"><tr><th>ID</th><th>Type</th><th>Summary</th><th>Status</th><th>User</th><th>Date</th><th>Hours</th></tr>';
+        return new Promise((resolve, reject) => {
+            let data = storage.report;
+            let holder = document.getElementById('holder');
+            let loading = document.getElementById('loading');
+            let total = 0;
+            let html = '<h3>Total hours for period: <span id="total"></span></h3><table id="report" class="table table-striped table-bordered table-hover"><tr><th>ID</th><th>Type</th><th>Summary</th><th>Status</th><th>User</th><th>Date</th><th>Hours</th></tr>';
 
-        for (let [key, value] of Object.entries(data)) {
-            if (key === 'total') {
-                total = value;
-            } else {
-                for (let [key, data] of Object.entries(value)) {
-                    const row = `<tr><td><a href="${storage.url}/browse/${key}" target="_blank" rel="noopener">${key}</a></td><td>${data.details.type}</td><td>${data.details.summary}</td><td>${data.details.status}</td>`;
-                    let thisDetails = '';
+            for (let [key, value] of Object.entries(data)) {
+                if (key === 'total') {
+                    total = value;
+                } else {
+                    for (let [key, data] of Object.entries(value)) {
+                        const row = `<tr><td><a href="${storage.url}/browse/${key}" target="_blank" rel="noopener">${key}</a></td><td>${data.details.type}</td><td>${data.details.summary}</td><td>${data.details.status}</td>`;
+                        let thisDetails = '';
 
-                    for (let log of data.data) {
-                        thisDetails += `${row}<td>${log.displayName}</td><td>${log.updated}</td><td>${log.timeSpentSeconds / 3600}</td></tr>`;
+                        for (let log of data.data) {
+                            thisDetails += `${row}<td>${log.displayName}</td><td>${log.updated}</td><td>${log.timeSpentSeconds / 3600}</td></tr>`;
+                        }
+
+                        html += thisDetails;
                     }
-
-                    html += thisDetails;
                 }
             }
-        }
 
-        html += '</tr></table>';
-        holder.innerHTML = html;
-        document.getElementById('total').innerHTML = parseInt(data.total) / 3600;
+            html += '</tr></table>';
+            holder.innerHTML = html;
+            document.getElementById('total').innerHTML = parseInt(data.total) / 3600;
 
-        loading.classList.add('bounceOut');
-        holder.classList.add('bounceIn');
-        holder.classList.remove('hide');
+            loading.classList.add('bounceOut');
+            holder.classList.add('bounceIn');
+            holder.classList.remove('hide');
+
+            resolve();
+        })
     };
 
     let getStorage = () => {
@@ -148,10 +166,65 @@
             'password': '',
             'jql': ''
         }, function (storage) {
-            getLoggedHours(storage).then(data => createReport(data));
+            getLoggedHours(storage).then(data => createReport(data).then(() => {
+
+                setTimeout(() =>{
+                    notify({
+                        icon: 'glyphicon-ok',
+                        type: 'info',
+                        title: 'Report is ready!',
+                        message: 'Remember that you can copy and paste the report into Excel if required.',
+                        url: storage.url + '/issues/?jql=' + storage.jiraReview,
+                    })
+                }, 1000)
+
+            }));
         })
     };
 
+    let notify = (config) =>{
+        $.notify({
+            // options
+            icon: 'glyphicon ' + config.icon,
+            title: config.title,
+            message: config.message,
+            url: config.url,
+            target: '_blank'
+        }, {
+            // settings
+            element: 'body',
+            type: config.type,
+            allow_dismiss: true,
+            newest_on_top: true,
+            showProgressbar: false,
+            placement: {
+                from: "top",
+                align: "right"
+            },
+            offset: 20,
+            spacing: 10,
+            z_index: 1031,
+            delay: 7000,
+            timer: 1000,
+            url_target: '_blank',
+            mouse_over: true,
+            animate: {
+                enter: 'animated fadeInDown',
+                exit: 'animated fadeOutUp'
+            },
+            icon_type: 'class',
+            template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+            '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+            '<span data-notify="icon"></span> ' +
+            '<span data-notify="title">{1}</span> ' +
+            '<div>&nbsp;</div><span data-notify="message">{2}</span>' +
+            '<div class="progress" data-notify="progressbar">' +
+            '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+            '</div>' +
+            '<a href="{3}" target="{4}" data-notify="url"></a>' +
+            '</div>'
+        });
+    };
 
     let init = () => {
         Flatpickr.l10ns.default.firstDayOfWeek = 1;
